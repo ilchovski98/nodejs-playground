dfb637b8b52b3942eb6720070257e354aa3b8de1
ilchovski98 <ilchovski98@gmail.com>
2022-09-07 20:55:29 +0300

    Load the html file once and serve it on request

--- app/htmlFile.js
+++ app/htmlFile.js
@@ -4,30 +4,25 @@ const fs = require('fs');
 const host = 'localhost';
 const port = 8000;
 
+let indexFile;
+
 const requestListener = (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.writeHead(200);
+  res.end(indexFile);
+}
 
-  fs.readFile(__dirname + '/index.html', (err, data) => {
-    res.end(data)
+const server = http.createServer(requestListener);
+
+fs.promises.readFile(__dirname + '/index.html')
+  .then(content => {
+    indexFile = content;
+
+    server.listen(port, host, () => {
+      console.log(`Server is listening on http://${host}:${port}`);
+    });
   })
-};
-
-const requestListener2 = (req, res) => {
-  fs.promises.readFile(__dirname + '/index.html')
-    .then(content => {
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead(200);
-      res.end(content);
-    })
-    .catch(error => {
-      res.writeHead(500);
-      res.end(error);
-    })
-};
-
-const server = http.createServer(requestListener2);
-
-server.listen(port, host, () => {
-  console.log(`Server is listening on http://${host}:${port}`);
-});
+  .catch(error => {
+    console.log("Couldn't load file!");
+    process.exit(1);
+  });
