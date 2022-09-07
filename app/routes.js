d01cbf1b4e763a81b03ae2a34ab5749b3e1844cd
ilchovski98 <ilchovski98@gmail.com>
2022-09-07 21:53:07 +0300

    Basic request routing implementation

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,38 @@
 const http = require('http');
-
-const host = 'localhost';
 const port = 8000;
+const host = 'localhost';
+
+const books = JSON.stringify([
+  { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+  { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+
+const authors = JSON.stringify([
+  { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+  { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
 
 const requestListener = (req, res) => {
-  res.writeHead(200);
-  res.end('My first server!');
+  res.setHeader('Content-Type', 'application/json');
+  console.log('req', req);
+
+  switch (req.url) {
+    case '/books':
+      res.writeHead(200);
+      res.end(books);
+      break;
+    case "/authors":
+      res.writeHead(200);
+      res.end(authors);
+      break
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({error:"Resource not found"}));
+  }
 }
 
 const server = http.createServer(requestListener);
 
 server.listen(port, host, () => {
-  console.log(`Server is listening on http://${host}:${port}`);
-});
+  console.log('Listening on port 8000');
+})
