a65e772b7f597e2e8f9b00a376528d3e71c6279c
ilchovski98 <ilchovski98@gmail.com>
2022-09-08 14:50:53 +0300

    Replace callback logic with promises

--- async/callbackMovies.js
+++ async/callbackMovies.js
@@ -1,31 +1,23 @@
+const axios = require('axios');
 const fs = require('fs');
-const request = require('request');
 
-request('https://ghibliapi.herokuapp.com/films', (error, response, body) => {
-  if (error) {
-    console.error(`Could not send request to API: ${error.message}`);
-    return;
-  }
-
-  if (response.statusCode != 200) {
-    console.error(`Expected status code 200 but received ${response.statusCode}.`);
-    return;
-  }
-
-  movies = JSON.parse(body);
+axios('https://ghibliapi.herokuapp.com/films')
+.then(response => {
+  console.log('Successfully retrieved our list of movies!');
 
   let movieList = '';
 
-  movies.forEach(movie => {
+  response.data.forEach(movie => {
     movieList += `${movie['title']}, ${movie['release_date']}\n`;
   });
 
-  fs.writeFile('moviesCsv.csv', movieList, error => {
-    if (error) {
-      console.log(`Could not save movies to the file: ${error}`);
-      return;
-    }
-
-    console.log('Saved movies in file');
-  })
+  return fs.promises.writeFile('moviesCsv.csv', movieList);
+})
+.then(() => {
+  console.log('Saved our list of movies to promiseMovies.csv');
+})
+.catch(error => {
+  if (error) {
+    console.error(`Could not send request to API: ${error.message}`);
+  }
 });
