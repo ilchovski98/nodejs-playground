23a036abd1e47b01ca3734649739fa1fe58f16b5
ilchovski98 <ilchovski98@gmail.com>
2022-09-10 17:26:43 +0300

    CRUD requests

---
+++ requests/getRequestWithGet.js
@@ -0,0 +1,25 @@
+const https = require('https');
+
+let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+    // Tells nodejs to not consume the data of the request (faster than garbage colleciton)
+    res.resume();
+    return;
+  }
+
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('close', () => {
+    console.log('Received all data.');
+    console.log(JSON.parse(data));
+  });
+});
+
+request.on('error', (err) => {
+  console.error(`Encountered an error trying to make a request: ${err.message}`);
+});
